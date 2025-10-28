d1a0cfe3acc0686c2de1ea63e7c3f55358be175f
Yravi prasad <ravix.prasad@intel.com>
2025-10-28 10:29:09 +0530

    update

--- mainExecutionScript.py
+++ mainExecutionScript.py
@@ -26,8 +26,9 @@ def main():
 
     error_logs = []
     error_logs_cmd = []
-    dpdk_dts_path  = ""
+    dpdk_dts_path = os.environ['DPDK_INSTALLTION_PATH']
     dpdk_dts_folder_name = "dts_setup"
+
     try:
         print("\n🚀 Starting Setup Scripts...\n")\
         # STEP 1: Define paths for firmware and driver packages
@@ -49,20 +50,21 @@ def main():
         script.setup_proxy_environment()
         
         # STEP 1.1: Update firmware and drivers
-        script.updating_firmware_drivers()
+        # script.updating_firmware_drivers()
 
         # STEP 1.2: Install required system and Python packages
         script.install_required_packages()
 
         # STEP 1.3: Prepare environment for DPDK/DTS setup
-        os.chdir("/root")
+        os.chdir(dpdk_dts_path)
         script.creating_folder_setup(dpdk_dts_folder_name)
+        os.chdir(dpdk_dts_folder_name)
 
-        pwd_path =script.run_command(["pwd"],"Fetching DPDK DTS Setup Path",check_output=True)
+        pwd_path = script.run_command(["pwd"],"Fetching DPDK DTS Setup Path",check_output=True)
         if pwd_path[0]:
             dpdk_dts_path = pwd_path[1]
         else:
-            dpdk_dts_path = f"/root/{dpdk_dts_folder_name}"
+            dpdk_dts_path = f"{dpdk_dts_path}/{dpdk_dts_folder_name}"
         
 
         # STEP 1.4: Clone DPDK and DTS repositories
@@ -74,32 +76,32 @@ def main():
         error_logs += script.error_logs
         error_logs_cmd += script.error_logs_cmd
 
-        # STEP 2: Fetch interface pairing info
-        print("🧩 Initializing PairingManagerInfo object...")
-        obj = PairingManagerInfo()
+        # # STEP 2: Fetch interface pairing info
+        # print("🧩 Initializing PairingManagerInfo object...")
+        # obj = PairingManagerInfo()
 
-        print("\n🔍 Fetching Interface and Bus Pairing Information...\n")
-        obj.fetchingInterFacePairingInfo()
+        # print("\n🔍 Fetching Interface and Bus Pairing Information...\n")
+        # obj.fetchingInterFacePairingInfo()
 
-        print("\n🔗 Fetching Interface Connection Details...\n")
-        obj.fetchingPairDetailsFromInterface()
+        # print("\n🔗 Fetching Interface Connection Details...\n")
+        # obj.fetchingPairDetailsFromInterface()
 
-        print("\nMapping Interface With Bus Info")
-        interface_details = obj.mapInterfaceToBus()
+        # print("\nMapping Interface With Bus Info")
+        # interface_details = obj.mapInterfaceToBus()
 
-        # STEP 3: Configure DUT ports
+        # # STEP 3: Configure DUT ports
        
-        ports_config_obj = DutPortConfig(dpdk_dts_path)
+        # ports_config_obj = DutPortConfig(dpdk_dts_path)
 
-        print(
-            "\n🔧 Loaded Configuration:\n"
-            "-----------------------------\n"
-            f"🌐 IP Address : {ports_config_obj.ip_address}\n"
-            f"👤 Username   : {ports_config_obj.username}\n"
-            f"🔑 Password   : {'*' * len(ports_config_obj.password) if ports_config_obj.password else 'Not Set'}\n"
-        )
+        # print(
+        #     "\n🔧 Loaded Configuration:\n"
+        #     "-----------------------------\n"
+        #     f"🌐 IP Address : {ports_config_obj.ip_address}\n"
+        #     f"👤 Username   : {ports_config_obj.username}\n"
+        #     f"🔑 Password   : {'*' * len(ports_config_obj.password) if ports_config_obj.password else 'Not Set'}\n"
+        # )
 
-        ports_config_obj.update_ports(interface_details)
+        # ports_config_obj.update_ports(interface_details)
 
     except Exception as e:
         print(f"\n❌ An error occurred during execution: {e}\n")
@@ -108,7 +110,4 @@ def main():
 
 
 if __name__ == "__main__":
-    # main()
-    print("Testing That Script Execution working fine")
-    print(os.environ['GIT_USERNAME'],os.environ['GIT_TOKEN'],os.environ['DRIVER_PATH'])
-
+    main()
