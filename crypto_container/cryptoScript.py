bffaf6e8606a69eec1ceec5a674a0a988195aec3
Yravi prasad <ravix.prasad@intel.com>
2025-11-11 09:32:14 +0530

    Update

--- crypto_container/cryptoScript.py
+++ crypto_container/cryptoScript.py
@@ -26,8 +26,11 @@ class CryptoSetupManager(CommonFuntion):
         self.git_token = git_token
         self.dts_url = f"https://{git_user}:{git_token}@github.com/intel-sandbox/networking.dataplane.dpdk.dts.local.upstream.git".replace(" ","")  #For cloning DTS after CREDENTIAL.
 
-        self.configFileName = "config_files"
-        self.qatDriverFolderName = "qat_driver"
+        self.git_url_intel_ipsec = "git clone https://github.com/intel/intel-ipsec-mb.git"
+        self.intel_ipsec_path = ""
+        self.config_file_name = "config_files"
+        self.qat_driver_folder_name = "qat_driver"
+        self.sw_crypto_driver_folder_name = "sw_crypto_driver"
 
     def clone_dts_repo(self):
 
@@ -40,12 +43,28 @@ class CryptoSetupManager(CommonFuntion):
         if "networking.dataplane.dpdk.dts.local.upstream" in directory:
             self.run_command(["rm","-rf","networking.dataplane.dpdk.dts.local.upstream"],"Removing Existing Cloning")
         print("\n📍current path : "+str(path))
+        
         self.run_command(["git", "clone", self.dts_url], "Cloning DTS repository")
         os.chdir("networking.dataplane.dpdk.dts.local.upstream")
         self.dts_setup_path =  os.getcwdb().decode()
         self.run_command(["git", "checkout","-b", "v25"], "Checking out DPDK version v25.03-rc3")
 
+    def clone_intel_ipsc_repo(self):
 
+        """
+        Clones the private DTS repository using GitHub credentials.
+        """
+       
+        path = os.getcwd()
+        directory = os.listdir()
+        print("\n📍current path : "+str(path))
+        
+        self.run_command(["git", "clone", self.git_url_intel_ipsec], "Cloning Intel IPSEC")
+        os.chdir("intel-ipsec-mb")
+        self.intel_ipsec_path = os.getcwdb().decode()
+        self.run_command(["git", "checkout","-b", "v25"], "Checkout for diffrent Branch")
+
+    
 
     def crypto_execution_script(self):
         """
@@ -100,7 +119,7 @@ class CryptoSetupManager(CommonFuntion):
                 # Ex/root/automation/config_files
 
                 # COPYING FILE :
-                cp_config_folder_path = os.path.join(self.automation_folder_path,self.configFileName)
+                cp_config_folder_path = os.path.join(self.automation_folder_path,self.config_file_name)
 
                 # It wil ensure If folder was not there then it will create folder 
                 # Going Within folder 
@@ -126,7 +145,8 @@ class CryptoSetupManager(CommonFuntion):
 
                 self.run_command(["ls",cp_config_folder_path],f"Showing file which is Copied : directory {cp_config_folder_path}")
                 self.print_separator("File Are Copied")
-            
+                # Now We are Copying File Which We will Process , Further.
+
             # GOING Within dep Folder for Taring DPDK EXECUTION SETUP 
             os.chdir(os.path.join(self.dts_setup_path,"dep"))
             self.print_separator(f"Current Path {str( os.getcwdb().decode() )}")
@@ -152,9 +172,10 @@ class CryptoSetupManager(CommonFuntion):
             self.run_command(["tar","-cvzf","dpdk.tar.gz","dpdk"])   
             self.run_command(["rm","-rf",dpdk_file_name], "Removing Extra file after Taring")
 
-
             ###########################################################STEP 2 PROCESS START #############################################################
-            cp_qat_driver_path = os.path.join(self.automation_folder_path,self.qatDriverFolderName)
+            
+            
+            cp_qat_driver_path = os.path.join(self.automation_folder_path,self.qat_driver_folder_name)
 
             qat_driver_file_name = os.path.basename(self.qat_driver_path)
             
@@ -164,9 +185,12 @@ class CryptoSetupManager(CommonFuntion):
                 self.run_command(["rm","-rf",qat_driver_file_name],"Removing If same file was there is QAT driver Folder")
             
             self.run_command(["cp",self.qat_driver_path,cp_qat_driver_path],f"Copying file QAT file := {qat_driver_file_name}")
-            
-            # Now We are Copying File Which We will Process , Further.
             print(os.getcwdb())
+            self.run_command(["tar","-xvf",qat_driver_file_name],f"Taring QAT driver flename : {qat_driver_file_name}")
+            # self.run_command(["make","uninstall"],"CMD : 'Make' doing Uninstall")
+            for file in os.listdir():
+                print(file)
+            
             return True
 
         except FileNotFoundError as e:
