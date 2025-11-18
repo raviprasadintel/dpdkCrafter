5741b60abc74dcfd28c3fbf1dbf1c46509beea5f
Yravi prasad <ravix.prasad@intel.com>
2025-11-18 10:49:40 +0530

    update

--- common_script_container/setup_installation.py
+++ common_script_container/setup_installation.py
@@ -42,7 +42,7 @@ class FirmwareDriverInstallation:
             
             os.chdir(firmware_name)
             CommonSetupCheck.print_separator(str(os.getcwd()))
-            
+            print(os.listdir())
 
 
 
