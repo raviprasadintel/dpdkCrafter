b6ff2ce2aa45168850164ef40028588084f87e4b
Yravi prasad <ravix.prasad@intel.com>
2025-11-18 11:21:30 +0530

    updte

--- common_script_container/setup_installation.py
+++ common_script_container/setup_installation.py
@@ -93,7 +93,7 @@ class FirmwareDriverInstallation:
             for file in os.listdir():
                 if (file in driver_file_name_before_tarting ) and (driver_file_name_before_tarting != file):
                     driver_name = file
-            print(os.listdir(),driver_name)
+            print(os.listdir(),driver_name,driver_file_name_before_tarting)
             os.chdir(driver_name)
             CommonSetupCheck.print_separator(str(os.getcwd()))
 
